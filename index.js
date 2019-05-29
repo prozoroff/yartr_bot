6f33149ae17c13a7971bab58d51b6094d8e698f2
alexprozoroff <alexprozoroff@gmail.com>
2019-05-29 08:20:14 +0000

    fixes

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.on('message', (msg) => {
   if (formattedMessage === 'волга') {
     const options = {
         reply_markup: JSON.stringify({
-          inline_keyboard: inRow(formatCallback(getDirections().map(d => [d, 'volga:' + d]), chatId).map(f => [f]),1)
+          inline_keyboard: inRow(formatCallback(getDirections().map(d => [d, 'volga:' + d]), chatId),1)
         })
       }
       return bot.sendMessage(chatId, messages.chooseDirection, options)
